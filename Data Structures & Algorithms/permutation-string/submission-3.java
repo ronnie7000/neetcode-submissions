36c36173b56c8a8fbb8373c4c8efd439d7f61e59
Akansh Nirmal <33548265+ronnie7000@users.noreply.github.com>
2026-06-11 01:49:16 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for(int i = 0; i < s1.length(); i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        int left = 0;
+
+        if(Arrays.equals(freq1, freq2)) {
+            return true;
+        }
+
+        for(int right = s1.length(); right < s2.length(); right++) {
+            freq2[s2.charAt(left) - 'a']--;
+            freq2[s2.charAt(right) - 'a']++;
+            left++;
+
+            if(Arrays.equals(freq1, freq2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
