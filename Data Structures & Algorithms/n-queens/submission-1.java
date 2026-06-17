c15295e4d50dbe2cdbc9bd8fcd244c4f12e5dde7
Akansh Nirmal <33548265+ronnie7000@users.noreply.github.com>
2026-06-17 12:36:07 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.java
@@ -0,0 +1,52 @@
+class Solution {
+    
+    List<List<String>> res = new ArrayList<>();
+    Set<Integer> col = new HashSet<>();
+    Set<Integer> diag = new HashSet<>();
+    Set<Integer> antiDiag = new HashSet<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(char[] row : board) {
+            Arrays.fill(row, '.');
+        }
+        
+        solve(board, 0);
+        return res;    
+    }
+
+    private void solve(char[][] board, int r) {
+        if(r == board.length) {
+            res.add(convertToString(board));
+            return;
+        }
+
+        for(int c = 0; c < board[0].length; c++) {
+            if(col.contains(c) || 
+               diag.contains(r - c) || 
+               antiDiag.contains(r + c)) {
+                continue;
+            }
+
+            board[r][c] = 'Q';
+            col.add(c);
+            diag.add(r - c);
+            antiDiag.add(r + c);
+
+            solve(board, r + 1);
+
+            board[r][c] = '.';
+            col.remove(c);
+            diag.remove(r - c);
+            antiDiag.remove(r + c);
+        }
+    }
+
+    private List<String> convertToString(char[][] board) {
+        List<String> list = new ArrayList<>();
+        for(char[] row : board) {
+            list.add(new String(row));
+        }
+        return list;
+    }
+}
