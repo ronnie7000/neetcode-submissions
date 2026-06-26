5d7ce11f9166e037bcd57da5d3b56585999acf48
Akansh Nirmal <33548265+ronnie7000@users.noreply.github.com>
2026-06-26 11:02:54 +0530

    Add: max-area-of-island - submission-4

---
+++ Data Structures & Algorithms/max-area-of-island/submission-4.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    private static final int[] dirX = new int[] {-1, 0, 1, 0};
+    private static final int[] dirY = new int[] {0, -1, 0, 1};
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int R = grid.length, C = grid[0].length;
+        boolean[][] visited = new boolean[R][C];
+        int maxArea = 0;
+
+        for(int row = 0; row < R; row++) {
+            for(int col = 0; col < C; col++) {
+                if(grid[row][col] == 1 && !visited[row][col]) {
+                    int area = dfs(grid, row, col, visited);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int row, int col, boolean[][] visited) {
+        visited[row][col] = true;
+        int area = 1;
+
+        for(int i = 0; i < dirX.length; i++) {
+            int x = row + dirX[i];
+            int y = col + dirY[i];
+
+            if(isValid(grid, x, y) && grid[x][y] == 1 && !visited[x][y]) {
+                area += dfs(grid, x, y, visited);
+            }
+        }
+        return area;
+    }
+
+    private boolean isValid(int[][] grid, int row, int col) {
+        if(-1 < row && row < grid.length && -1 < col && col < grid[0].length) {
+            return true;
+        }
+        return false;
+    }
+}
