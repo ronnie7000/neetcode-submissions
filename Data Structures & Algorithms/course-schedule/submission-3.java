c7909af443108a8ae5c2e33e8209047180b26b79
Akansh Nirmal <33548265+ronnie7000@users.noreply.github.com>
2026-06-28 12:12:11 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> graph = getGraph(numCourses, prerequisites, indegree);
+        Queue<Integer> queue = new LinkedList<>();
+        int[] ans = new int[numCourses];
+        int idx = 0;
+
+        for(int i = 0; i < indegree.length; i++) {
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int course = queue.poll();
+            ans[idx++] = course;
+
+            for(int next : graph.get(course)) {
+                indegree[next]--;
+                if(indegree[next] == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+
+        return idx == numCourses;
+    }
+
+    private List<List<Integer>> getGraph(int numCourses, int[][] arr, int[] indegree) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] edge : arr) {
+            int course = edge[0];
+            int pre = edge[1];
+            graph.get(pre).add(course);
+            indegree[course]++;
+        }
+
+        return graph;
+    }
+}
