6a6e3ae0ee140ee9149d696cae1a10eec3624831
Akansh Nirmal <33548265+ronnie7000@users.noreply.github.com>
2026-08-02 13:09:54 +0530

    Add: can-place-flowers - submission-2

---
+++ Data Structures & Algorithms/can-place-flowers/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int idx = 0, count = 0;
+
+        while(idx < flowerbed.length) {
+            int prev = getSafe(flowerbed, idx - 1);
+            int next = getSafe(flowerbed, idx + 1);
+
+            if(flowerbed[idx] == 0 && prev != 1 && next != 1) {
+                flowerbed[idx] = 1;
+                count++;
+            }
+            idx += 2;
+            
+            if(count >= n) {
+                return true;
+            }
+        }
+        return count >= n;
+    }
+
+    private int getSafe(int[] arr, int pos) {
+        if(-1 < pos && pos < arr.length) {
+            return arr[pos];
+        }
+        return 0;
+    }
+}
